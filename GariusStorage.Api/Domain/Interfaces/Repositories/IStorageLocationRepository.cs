eabe863cc8fd341bbe0be4c3760f749e0c3d1164
George Souza <georgelucas.souza@outlook.com>
2025-05-20 20:46:57 -0300

    ...

---
+++ GariusStorage.Api/Domain/Interfaces/Repositories/IStorageLocationRepository.cs
@@ -0,0 +1,8 @@
+﻿using GariusStorage.Api.Domain.Entities;
+
+namespace GariusStorage.Api.Domain.Interfaces.Repositories
+{
+    public interface IStorageLocationRepository : IRepository<StorageLocations>
+    {
+    }
+}
